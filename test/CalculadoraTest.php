207ee008ee57597ff749adb83976abe914261ce0
Vicente <vmonfort@florida-uni.es>
2024-01-09 12:34:19 +0100

    inicializando

---
+++ test/CalculadoraTest.php
@@ -0,0 +1,13 @@
+<?php
+use PHPUnit\Framework\TestCase;
+include './src/Calculadora.php';
+
+final class CalculadoraTest extends TestCase
+{
+    public function testSuma()
+    {
+        $calc = new Calculadora(3,5);
+        $this->assertEquals(8, $calc->suma());
+    }
+}
+?>
